a30c2f408869994cf3db6c4b82f193986c0a8533
Gaurav <gaurav.sahu@clouwood.com>
2021-08-26 07:34:29 +0530

    first commit2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,151 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import Pictures from './Picture';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="header">
+        <div className="left-header">
+          <img src="https://www.collegedisha.com/assets/images/college-dishalogo.svg" alt="img" />
+        </div>
+        <div className="center-header">
+          <div>
+            <p>Why College Disha</p>
+          </div>
+          <div>
+            <p>Colleges By Stream</p>
+          </div>
+          <div>
+            <p>Admission 2021</p>
+          </div>
+          <div>
+            <p>Explore</p>
+          </div>
+          <div>
+            <p>Plans</p>
+          </div>
+        </div>
+        <div className="right-header">
+          <div className="header-icon">
+            <i class="fa fa-search"></i>
+          </div>
+          <div className="login">
+            <div className="locker">
+              <i class="fa fa-lock"></i>
+            </div>
+            <div>
+              <p>Login /</p>
+            </div>
+            <div className="contact">
+
+              <i class="fa fa-user"></i>
+            </div>
+            <div>
+              <p>Register</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section1">
+        <div className="section1-right">
+          <div className="main-cont">
+            <p>Find Top <span style={{color: "#213858"}}><b>Colleges, <br />Coaching, Courses..!</b></span></p>
+          </div>
+          <div className="search-bar">
+            <div style={{ marginLeft: 15 }}>
+              <p>Search Colleges, Courses, Coaching</p>
+            </div>
+            <div className="search">
+
+              <i class="fa fa-search"></i>
+            </div>
+          </div>
+        </div>
+        <div className="section-left">
+          <div className="image">
+            <img alt="img" src="https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FcounsellingCard.webp&w=384&q=75" />
+          </div>
+          <div className="second-text">
+            <p>Go for the Counselling</p>
+          </div>
+          <div className="second-button">
+            <button>Click Here</button>
+          </div>
+
+        </div>
+      </div>
+      <div className="section2">
+        <div className="section2-content">
+          <h1>Our Features</h1>
+          <p>College Disha will guide you to evaluate your strengths and explore the best<br /> career options for a rewarding future.</p>
+        </div>
+        <div className="parent-card">
+          {
+            Pictures.map((val, ind) => {
+              return (
+                <div className="main-card" key={ind}>
+                  <div className="image-card">
+                    <img src={val.imgsrc} alt="img"/>
+                  </div>
+                  <div className="card-title">
+                    <p>{val.title}</p>
+                  </div>
+                  <div className="card-content">
+                    <p>{val.caption}</p>
+                  </div>
+                </div>
+              )
+            })
+          }
+
+
+        </div>
+        <div className="section3">
+          <div className="section3-title">Fill Your Details for Counselling</div>
+          <div className="section3-parts">
+            <div className="section3-left">
+              <div>
+                <img src="https://www.collegedisha.com/assets/images/for-student.webp" alt="img"/>
+              </div>
+              <div className="left2">
+                <p>Fill up the form and our mentors will help you overcome career related problems with smart solutions.</p>
+              </div>
+              <div className="left3">
+                <button>Contact Us</button>
+              </div>
+            </div>
+            <div className="section3-right">
+              <div className="section-right-title">
+                <h2>Fill the Details Here</h2>
+              </div>
+              <div className="section-right-subcontent">
+                <p>Student / Parent</p>
+              </div>
+              <div className="input">
+                <input placeholder="Name" />
+                <input placeholder="Phone Number" />
+                <input placeholder="Email" />
+                <input placeholder="Last Education" />
+                <input placeholder="Current Location" />
+                <input placeholder="Preferred Location" />
+                <input placeholder="Guardian's Name" />
+                <input placeholder="Gaurdian's Phone Number" />
+              </div>
+              <div className="submit-line">
+              <div>
+                <p>Are you a parent?Click Here</p>
+              </div>
+              <div>
+                <button>Submit</button>
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Picture.js
@@ -0,0 +1,45 @@
+
+const Pictures = [
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-1.webp&w=256&q=75",
+        title: "Course Counselling",
+        caption: "Find the suitable course to pursue with the help of our psychometric test and expert counsellors."
+    },
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-2.webp&w=256&q=75",
+        title: "College Counselling ",
+        caption: "Get admission in the best college as per your course choice."
+    },
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-3.webp&w=256&q=75",
+        title: "Guidance from Professionals",
+        caption: "Reach out to our industry experts to clear all doubts related to your career."
+    },
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-4.webp&w=256&q=75",
+        title: "Internship Program",
+        caption: "Get a free Internship on trending technology/language in a reputed institute/company."
+    },
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-5.webp&w=256&q=75",
+        title: "Off Campus Placement",
+        caption: "If you don’t get placement through the college,  get placed in MNCs through us."
+    },
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-6.webp&w=256&q=75",
+        title: "Job Guarantee Card",
+        caption: "We commit  to provide  job assistance for the next five years after your course."
+    },
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-7.webp&w=256&q=75",
+        title: "Listed Top Colleges",
+        caption: "Get Admission in Top Colleges with the help of our college counsellors."
+    },
+    {
+        imgsrc: "https://www.collegedisha.com/_next/image?url=%2Fassets%2Fimages%2FfeatureImages%2Ffeature-8.webp&w=256&q=75",
+        title: "Listed Best Courses",
+        caption: "Get Admission in Best Course with the help of our course counsellors."
+    },
+]
+
+export default Pictures;
